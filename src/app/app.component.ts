7575fc3aedbad368aca71154e116055ec7cf9d6d
shashank <shashank944533@gmail.com>
2023-01-25 12:09:36 +0530

    just for execution

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Demo message</h1>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {}
